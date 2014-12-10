806741529c1cc18f2fe2e6c28dbc60ae82a045a7
rm207 <isda@udayton.edu>
2014-12-10 13:27:47 -0500

    added function to style the searchtool

--- search_form.js
+++ search_form.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-function() {
+function search_form_style() {
 	//modify some of the elements in our top search form to enable responsive function
 	if (document.getElementsByName('searchtool')) {	
 		//remove the border around the searchtool
@@ -16,7 +16,7 @@ function() {
 		//change the input values so that they have the proper class name 
 		document.getElementById('searchtype').className = 'form-control';
 		document.getElementById('searcharg').className = 'form-control';
-		document.getElementById('sortdropdown').className = 'form-control';
+		document.getElementById('sortdropdown').className += ' form-control';
 		
 		//wrap the input element for the checkbox in a div and label
 		var checkbox_label = document.getElementsByClassName('availLimMessage')[0];
@@ -31,5 +31,5 @@ function() {
 			document.getElementById('sortbutton').className = 'btn btn-default';
 		}//end if
 	}//end if
-};
+}
 </script>
